cb258922a0906ce0ab3691183c5ff3b027edaefd
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-08-22 17:11:54 +0100

    ajustando o resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
         <div className="max-w-5xl mx-auto p-6 bg-white rounded mt-2">
 
             <div className="mb-6">
-                <h3 className="text-xl font-semibold">Web Developer – ForLearn</h3>
+                <h3 className="text-xl font-semibold">FrontEnd Developer – ForLearn</h3>
                 <p className="text-sm text-gray-600">2024</p>
                 <ul className="list-disc list-inside mt-2 text-gray-700">
                     <li>Developed interfaces using React + Next.js and TailwindCSS.</li>
